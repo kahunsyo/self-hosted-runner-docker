6aea04bcd5fc763a2b021cdf21a1348e71f7140b
ichino <81274957+kahunsyo@users.noreply.github.com>
2025-05-17 12:00:36 +0900

    feat: start up script

--- README.md
+++ README.md
@@ -11,8 +11,25 @@ Following permissions are required.
   - admin:org
     - manage_runners:org
 
-## Removing Offline runners
+## Removing offline runners
 
 Runner is automatically removed from GitHub if it has not connected to GitHub Actions for more than 1 day.
 
 To detail: https://docs.github.com/en/actions/hosting-your-own-runners/managing-self-hosted-runners/removing-self-hosted-runners
+
+## Start runner
+
+```shell
+# Set unique docker compose project name when multi-runners in one host
+# Default project is "runner"
+PROJECT='' sh start.sh
+```
+
+### Stop runner
+
+Stop start.sh script and stop runner project.
+
+```shell
+PROJECT=''
+docker compose -p $PROJECT down --remove-orphans --volumes
+```
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ set -eux
 sudo chown -R runner:runner /home/runner/_work
 sudo chown -R runner:runner /home/runner/actions-runner
 
-export RUNNER_TOKEN=$(curl -s -X POST ${BASE_API_URL}/${ORGS_OR_REPOS}/${RUNNER_SCOPE}/actions/runners/registration-token -H "accept: application/vnd.github.everest-preview+json" -H "authorization: token ${RUNNER_CFG_PAT}" | jq -r '.token')
+export RUNNER_TOKEN=$(curl -s -X POST "${BASE_API_URL}/${ORGS_OR_REPOS}/${RUNNER_SCOPE}/actions/runners/registration-token" -H "accept: application/vnd.github.everest-preview+json" -H "authorization: token ${RUNNER_CFG_PAT}" | jq -r '.token')
 
 unset GITHUB_ACCESS_TOKEN # Security
 
-/home/runner/config.sh --url https://github.com/kahunsyo/self-hosted-runner-docker --token $RUNNER_TOKEN --runnergroup default --name $HOSTNAME --no-default-labels --work _work --disableupdate  --ephemeral --labels 'self-hosted'
+/home/runner/config.sh --unattended --url "https://github.com/${RUNNER_SCOPE}" --token "${RUNNER_TOKEN}" --runnergroup default --name "${HOSTNAME}" --work _work --disableupdate --ephemeral --replace --labels "${RUNNER_LABELS}"
 /home/runner/run.sh
